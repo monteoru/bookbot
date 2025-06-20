b947dc763d5118043c5a9ca810497cfb00f5a94e
RĂZVAN ILIE <i@ra.mt>
2025-06-20 21:49:34 +0300

    Add final files for bookbot

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from stats import get_num_words, return_count_of_characters
+import sys
+from ntpath import extsep
 
 def get_book_text(file_path):
    with open(file_path) as f:
         return f.read()
 
 def main():
-    path = './books/frankenstein.txt'
-    #print(get_book_text(path))
+    path = sys.argv[1]
+
     text = get_book_text(path)
     num_words = len(get_num_words(text))
-    print(f'{num_words} words found in the document')
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {num_words} total words')
+    print('----------- Character Count ----------')
+    result = return_count_of_characters(text)
 
-    print(return_count_of_characters(text))
-main()
+    for item in result:
+        print(f'{item["char"]}: {item["num"]}')
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    main()
--- stats.py
+++ stats.py
@@ -1,5 +1,3 @@
-from pickle import EMPTY_DICT
-from string import printable
 def get_num_words(text):
     return text.split()
 
@@ -12,4 +10,9 @@ def return_count_of_characters(text):
             total_of_characters[character] += 1
         else:
             total_of_characters[character] = 1
-    return total_of_characters
+
+    result = []
+    for char, count in total_of_characters.items():
+        result.append({'char': char, 'num': count})
+    return sorted(result, key=lambda x: x['num'], reverse=True)
+    #return total_of_characters
